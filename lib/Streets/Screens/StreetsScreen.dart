afc4f73653ec74799f89d2d91c397bdb849003ed
AbanoubSamy <abanoubsam1999@gmail.com>
2021-07-19 11:18:40 +0200

    Update Upload to Google play

--- lib/Streets/Screens/StreetsScreen.dart
+++ lib/Streets/Screens/StreetsScreen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:the_world_around_you_user/Cities/Widgets/CitiesItem.dart';
 import 'package:the_world_around_you_user/Sections/screens/SectionScreen.dart';
 import 'package:the_world_around_you_user/Streets/Controller/AreaController.dart';
 import 'package:the_world_around_you_user/Streets/Model/AreaModel.dart';
-import 'package:the_world_around_you_user/Streets/Widgets/StreetsItem.dart';
 
 class StreetsScreen extends StatefulWidget {
   String CitName;
@@ -18,6 +16,7 @@ class _StreetsScreenState extends State<StreetsScreen> {
   AreaController _areaController= AreaController();
   AreaModel _areaModel=AreaModel();
   String _area;
+  List<Color> colors = [Colors.red, Colors.green, Colors.yellow,Colors.blueAccent,Colors.orange,Colors.brown, Colors.black87, Colors.cyanAccent,Colors.deepPurple,Colors.yellowAccent,Colors.red[100], Colors.green[100], Colors.yellow[100],Colors.blueAccent[100],Colors.orange[100],Colors.brown[100], Colors.black87, Colors.cyanAccent[100],Colors.deepPurple[100],Colors.yellowAccent[100],Colors.red[200], Colors.green[200], Colors.yellow[200],Colors.blueAccent[200],Colors.orange[200],Colors.brown[200], Colors.black87, Colors.cyanAccent[200],Colors.deepPurple[200],Colors.yellowAccent[200],Colors.red[300], Colors.green[300], Colors.yellow[300],Colors.blueAccent[300],Colors.orange[300],Colors.brown[300], Colors.black87, Colors.cyanAccent[300],Colors.deepPurple[300],Colors.yellowAccent[300],Colors.red[400], Colors.green[400], Colors.yellow[400],Colors.blueAccent[400],Colors.orange[400],Colors.brown[400], Colors.black87, Colors.cyanAccent[400],Colors.deepPurple[400],Colors.yellowAccent[400],Colors.red[500], Colors.green[500], Colors.yellow[500],Colors.blueAccent[500],Colors.orange[500],Colors.brown[500], Colors.black87, Colors.cyanAccent[500],Colors.deepPurple[500],Colors.yellowAccent[500],Colors.red[600], Colors.green[600], Colors.yellow[600],Colors.blueAccent[600],Colors.orange[600],Colors.brown[600], Colors.black87, Colors.cyanAccent[600],Colors.deepPurple[600],Colors.yellowAccent[600],Colors.red, Colors.green, Colors.yellow,Colors.blueAccent,Colors.orange,Colors.brown, Colors.black87, Colors.cyanAccent,Colors.deepPurple,Colors.yellowAccent,Colors.red, Colors.green, Colors.yellow,Colors.blueAccent,Colors.orange,Colors.brown, Colors.black87, Colors.cyanAccent,Colors.deepPurple,Colors.yellowAccent,Colors.red, Colors.green, Colors.yellow,Colors.blueAccent,Colors.orange,Colors.brown, Colors.black87, Colors.cyanAccent,Colors.deepPurple,Colors.yellowAccent,Colors.red, Colors.green, Colors.yellow,Colors.blueAccent,Colors.orange,Colors.brown, Colors.black87, Colors.cyanAccent,Colors.deepPurple,Colors.yellowAccent];
 
   Stream<AreaModel>_getArea () async*{
     _areaModel=await _areaController.GetArea(id: widget.CityID);
--- lib/const.dart
+++ lib/const.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyConstants extends InheritedWidget {
-  final   String urlBasic="http://theworldaroundu-001-site1.dtempurl.com/TheWoldAroundWS.svc/";
+  final   String urlBasic="http://theworldaroundu2-001-site1.btempurl.com/TheWoldAroundWS.svc/";
 
   @override
   bool updateShouldNotify(MyConstants oldWidget) => false;
